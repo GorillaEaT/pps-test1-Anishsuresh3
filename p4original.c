9067761df6bfb63a73f4efd1d84a6bff70ec3208
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2021-12-22 05:10:58 +0000

    .

---
+++ p4original.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("Enter the number\n");
+  scanf("%d",&a);
+  return a;
+}
+int cmp(int a,int b,int c)
+{
+  
+  if(a<b && c<b)
+     return b;
+  else if(a<c)
+     return c;
+  else
+     return a;
+}
+int cmp1(int a,int b,int c)
+{
+  int l;
+  l=(a>b)?(a>c?a:c):(b>c?b:c);
+  return l;
+}
+int main()
+{
+  int a,b,c;
+  a=input();
+  b=input();
+  c=input();
+  printf("Largest value: %d\n",cmp1(a,b,c));
+  return 0;
+}
